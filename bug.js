8023971de1b5b7bb60e7761033e77380bb4a149f
BugHunter-X <askzenai@gmail.com>
2024-12-14 18:30:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+// some tailwind classes that may not work correctly
+<div class="flex space-x-2">
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Button
+  </button>
+  <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+    Another button
+  </button>
+</div>
+```
